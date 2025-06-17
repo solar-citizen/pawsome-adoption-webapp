e5ea206b24e3f3caf072b7f04723a0b7ad2c5abb
Vladyslav Tishchenko <fsknnn@gmail.com>
2025-06-17 17:03:16 +0300

    Update configs

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,27 +1,31 @@
 import js from '@eslint/js';
 import ts from '@typescript-eslint/parser';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tsEslint from 'typescript-eslint';
-import react from 'eslint-plugin-react';
+import type { ESLint } from 'eslint';
 import prettier from 'eslint-config-prettier';
 import esLintImport from 'eslint-plugin-import';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import tsEslint from 'typescript-eslint';
 
 const customRules = {
   /* eslint */
   'max-len': ['warn', { code: 100 }],
   semi: [2, 'always'],
-  /* eslint-plugin-simple-import-sort */
+
+  // eslint-plugin-simple-import-sort
   'simple-import-sort/imports': 'error',
   'simple-import-sort/exports': 'error',
-  /* eslint-plugin-import */
+
+  // eslint-plugin-import
   'import/first': 'error',
   'import/newline-after-import': 'error',
   'import/no-duplicates': 'error',
   'import/max-dependencies': ['warn', { max: 10, ignoreTypeImports: true }],
-  /* no-unused-vars */
+
+  // no-unused-vars
   '@typescript-eslint/no-unused-vars': 'error',
   'no-unused-vars': 'off',
 };
@@ -37,7 +41,7 @@ const tsConfig = {
   files: ['**/*.{ts,tsx}'],
   languageOptions: {
     parser: ts,
-    ecmaVersion: 2020,
+    ecmaVersion: 2024,
     globals: {
       ...globals.browser,
       ...globals.node,
@@ -57,7 +61,7 @@ const tsConfig = {
     'simple-import-sort': simpleImportSort,
     'react-hooks': reactHooks,
     'react-refresh': reactRefresh,
-    import: esLintImport,
+    import: esLintImport as ESLint.Plugin,
     react,
   },
   rules: {
